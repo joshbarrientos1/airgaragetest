8fc1e58c2ad589c51ea5a96dbae7d1f7a7d7a0b5
joshbarrientos1 <joshbarrientos1@gmail.com>
2021-04-20 18:05:42 -0400

    Everything but arithmitic

--- parkingapp/views.py
+++ parkingapp/views.py
@@ -1,7 +1,12 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from .services import *
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
 def index(request):
-    return render(request, 'parkingapp/index.html', {})
+    location = request.POST.get('location_button')
+    businesses = []
+    businesses = yelp_call(location=location)
+    return render(request, 'parkingapp/index.html', {'results':businesses,})
